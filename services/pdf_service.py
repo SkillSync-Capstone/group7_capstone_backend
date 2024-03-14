ccfe0739976b4301d75e5ae7f5e92a5debfe700e
kevinPatel11 <kevinpatel1142@gmail.com>
2024-03-14 11:50:59 -0400

    Refector text chunk code

--- services/pdf_service.py
+++ services/pdf_service.py
@@ -3,6 +3,7 @@ from services import vectorStorageService
 from utils.text_chunk import chunk_text
 from services import llmService,text_service
 from typing_extensions import Concatenate
+from langchain.text_splitter import CharacterTextSplitter
 
 PINECONE_INDEX_NAME = 'llm'
 
@@ -10,12 +11,19 @@ def handle_pdf(pdf_file):
     pdf = PdfReader(pdf_file)
     raw_text = ''
     for i, page in enumerate(pdf.pages):
-     content = page.extract_text()
-    if content:
-        raw_text += content
+        content = page.extract_text()
+        if content:
+         raw_text += content
+    text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 800,
+    chunk_overlap  = 200,
+    length_function = len,
+)
+    texts = text_splitter.split_text(raw_text)
         
     chunks = chunk_text(raw_text)
-    vectorStorageService.embed_chunks_and_upload_to_pinecone(chunks, PINECONE_INDEX_NAME)
+    vectorStorageService.embed_chunks_and_upload_to_pinecone(texts, PINECONE_INDEX_NAME)
     response_json = {
         "message": "Chunks embedded and stored successfully"
     }
