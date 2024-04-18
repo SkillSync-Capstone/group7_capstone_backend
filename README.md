73c04e17a42a188eff22cca19de0d6017bcec61a
Damodar Subedi <damodarsubedi2015@gmail.com>
2024-04-18 00:24:20 -0400

    Added the cors policy in the appp

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # group7_capstone_backend
+
 backend for capstone project
-
-
-
 
 Run project usin python app.py
 [Flow Chart (1).pdf](https://github.com/SkillSync-Capstone/group7_capstone_backend/files/14527376/Flow.Chart.1.pdf)
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from dotenv import load_dotenv
 from routes.routes import api_routes
+from flask_cors import CORS
 
 load_dotenv()  # Load environment variables
 
 app = Flask(__name__)
+CORS(app)
+
 
 # Register the Blueprint from routes.py
 app.register_blueprint(api_routes)
